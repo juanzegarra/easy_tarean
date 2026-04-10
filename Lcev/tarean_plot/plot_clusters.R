a46c17739aedae8cf58801e8446edfa4d4a1f931
juanzegarra <juanzegarra71@gmail.com>
2026-04-10 15:14:13 -0300

    Adding pizza plot to --plot, bug correction

--- Lcev/tarean_plot/plot_clusters.R
+++ Lcev/tarean_plot/plot_clusters.R
@@ -3,33 +3,87 @@
 library(dplyr)
 library(ggplot2)
 library(viridis)
+library(hash)
 
 args <- commandArgs(trailingOnly = TRUE)
-input_file <- args[1]
-output_file <- args[2]
+input_clusters <- args[1]
+input_stats <- args[2]
+output_clusters <- args[3]
+output_stats <- args[4]
 
-df <- read.table(input_file, header = TRUE, sep = "\t")
+df <- read.table(input_clusters, header = TRUE, sep = "\t")
+df2 <- read.table(input_stats, header = TRUE, sep = "\t")
 
-df_sum <- df[order(df$Number.of.reads), ]
+df_sum <- df[order(-df$Number.of.reads), ]
+
+ann_colors <- c(
+  "LTR" = "#440154",
+  "LINE" = "#ff10e3",
+  "SINE" = "#21918C",
+  "Helitron" = "#5DC863",
+  "Mutator" = "#277DA1",
+  "hAT" = "#F8961E",
+  "TcMar" = "#F3722C",
+  "PiggyBac" = "#F94144",
+  "PIF-Harbinger" = "#9B5DE5",
+  "CMC" = "#577590",
+  "P" = "#43AA8B",
+  "MITE" = "#4D908E",
+  "TIR" = "#90BE6D",
+  "Organelle" = "#FDE725",
+  "rDNA" = "#B56576",
+  "NA" = "#898989"
+)
+
+df_sum$Annotation[df_sum$Annotation == "" |
+                     is.na(df_sum$Annotation) |
+                     df_sum$Annotation == "unknown"] <- "NA"
+
+df_sum$Annotation[is.na(df_sum$Annotation)| df_sum$Annotation=="<NA>"] <- "NA"
+
+df_sum$Cluster <- factor(df_sum$Cluster, levels = df_sum$Cluster)
+
+df_sum$Annotation <- factor(df_sum$Annotation, levels = names(ann_colors))
+df_sum$Annotation[is.na(df_sum$Annotation)] <- "NA"
 
 p <- ggplot(df_sum, aes(
-    x = Cluster,
-    y = Number.of.reads,
-    fill = Annotation
+  x = Cluster,
+  y = Number.of.reads,
+  fill = Annotation
 )) +
-  geom_col(position = "identity", width = 0.64) +
+  geom_col() +
   labs(
-    title = element_blank(),
-    x = element_blank(),
+    x = "Clusters",
     y = "Cluster size (n° of reads)"
   ) +
   theme(
     panel.background = element_rect(fill = "#F5F5F5"),
     panel.grid = element_blank(),
-    legend.position = "none",
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 8, face = "bold"),
-    axis.ticks = element_blank()
+    axis.text.x = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.y = element_text(size = 10, face = "bold", color = "black")
   ) +
-  scale_fill_viridis_d(option = "plasma")
+  scale_fill_manual(values = ann_colors, drop = TRUE)
 
-ggsave(output_file, p, width = 8, height = 6, dpi = 300)
+q <- ggplot(df2, aes(x = "", y = Proportion, fill = Type)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar(theta = "y") +
+      geom_text(
+        aes(label = ifelse(Proportion > 1.0,
+                   sprintf("%.1f", Proportion),
+                   "")),
+        position = position_stack(vjust = 0.5),
+        color = "black", size = 5,
+        fontface = "bold") +
+    labs(labs(title = "", x = "", y = "")) +
+    theme(
+        panel.background = element_rect(fill = "white"),
+        legend.title = element_blank(),
+        legend.text = element_text(colour = "black", face = "bold", size = 15),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank()
+    )+
+  scale_fill_manual(values = ann_colors, drop = TRUE)
+
+ggsave(output_stats, q, width = 8, height = 6, dpi = 300)
--- Lcev/tarean_plot/tarean_parser.py
+++ Lcev/tarean_plot/tarean_parser.py
@@ -5,27 +5,61 @@ import argparse
 from collections import defaultdict
 from bs4 import BeautifulSoup
 import subprocess
+import os
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Easy post-processing of TAREAN html report")
+        description="Easy post-processing of TAREAN html report"
+    )
     parser.add_argument("html_file", help="Path to the input html file")
-    parser.add_argument("-o", type=str, default="output", help="Base name for output directory and files")
-    parser.add_argument("--g_size", type=int, default=180000000, help="Genome size for estimated copy number calculation (default for average Drosophila genome size)")
-    parser.add_argument("--plot", action="store_true", help="Plot clusters by size (number of reads) and annotation")
-    parser.add_argument("--repeatM_file", type=str, help="path to custom annotation file")
-    parser.add_argument("--custom_anno", action="store_true", help="Enable overwriting of TAREAN annotation for a external annotation file,\n more information about annotation file format on read.me")
-    parser.add_argument("--custom_sat", action="store_true", help="Enable use of custom satDNA fasta file that annotates (note that it must be a exact sequence match)")
-    parser.add_argument("--fasta_sats", type=str, help="path to custom satDNA file")
+    parser.add_argument(
+        "-o",
+        type=str,
+        default="output",
+        help="Base name for output directory and files",
+    )
+    parser.add_argument(
+        "--g_size",
+        type=int,
+        default=180000000,
+        help="Genome size for estimated copy number calculation (default for average Drosophila genome size)",
+    )
+    parser.add_argument(
+        "--plot",
+        action="store_true",
+        help="Plot clusters by size (number of reads) and annotation",
+    )
+    parser.add_argument(
+        "--repeatM_file", type=str, help="path to custom annotation file"
+    )
+    parser.add_argument(
+        "--custom_anno",
+        action="store_true",
+        help="Enable overwriting of TAREAN annotation by an external annotation file,\n more information about annotation file format on read.me",
+    )
+    parser.add_argument(
+        "--custom_sat",
+        action="store_true",
+        help="Enable use of custom satDNA fasta file that annotates (note that it must be a exact sequence match)",
+    )
+    parser.add_argument(
+        "--fasta_sats", type=str, help="path to custom satDNA file"
+    )
     return parser.parse_args()
+
 
 class TAREAN_post_processing:
     def __init__(self, html_file, output_base, g_size):
         self.html_file = html_file
         self.output_base = output_base
         self.g_size = g_size
-        #set for excluding other types of headers
-        self.types = {" Putative satellites (high confidence)", " Putative satellites (low confidence)", " Other"}
+        # set for excluding other types of headers
+        self.types = {
+            " Putative satellites (high confidence)",
+            " Putative satellites (low confidence)",
+            " Other",
+        }
         self.clusters, self.metrics = self.parse_html(html_file)
 
     def parse_html(self, html_file):
@@ -33,19 +67,29 @@ class TAREAN_post_processing:
         with open(html_file, "r") as f:
             html_content = f.read()
             soup = BeautifulSoup(html_content, "html.parser")
-            number_of_reads = int(re.search(r'Number of input reads: (\d+)', html_content).group(1))
+            number_of_reads = int(
+                re.search(r"Number of input reads: (\d+)", html_content).group(
+                    1
+                )
+            )
             metrics["Number of input reads"] = number_of_reads
-            analyzed_reads = int(re.search(r'Number of analyzed reads: (\d+)', html_content).group(1))
+            analyzed_reads = int(
+                re.search(
+                    r"Number of analyzed reads: (\d+)", html_content
+                ).group(1)
+            )
             metrics["Number of analyzed reads"] = analyzed_reads
             clusters = []
-            #iterates over each in h2 header that delimitates tables
+            # iterates over each in h2 header that delimitates tables
             for h2 in soup.find_all("h2"):
                 if h2.text in self.types:
                     table = h2.find_next("tbody")
-                    #excludes header line of the table
-                    for row in table.find_all("tr", class_=lambda x: x != "firstline"):
+                    # excludes header line of the table
+                    for row in table.find_all(
+                        "tr", class_=lambda x: x != "firstline"
+                    ):
                         cluster = defaultdict(int)
-                        #divides row and extracts relevant cells
+                        # divides row and extracts relevant cells
                         c = row.find("td", class_="firstcolumn")
                         cells = row.find_all("td", class_="cellinside")
                         cluster["Cluster"] = c.text.strip()
@@ -57,21 +101,27 @@ class TAREAN_post_processing:
                         P_index = cells[10].text.strip()
                         annotation_cell = cells[15]
 
-                        #extracts highest ranking annotation
+                        # extracts highest ranking annotation
                         clean_annotation = annotation_cell.find_all("b")
                         max_annotation = defaultdict(int)
                         annotation = ""
                         for i in clean_annotation:
                             if len(clean_annotation) <= 1:
-                                annotation = clean_annotation[0].text.split("%")[1]
+                                annotation = clean_annotation[0].text.split(
+                                    "%"
+                                )[1]
                             elif not i.text:
-                              annotation = "NA"
+                                annotation = "NA"
                             else:
                                 annotation_value = float(i.text.split("%")[0])
                                 annotation_name = i.text.split("%")[1]
-                                max_annotation[annotation_name] = annotation_value
+                                max_annotation[annotation_name] = (
+                                    annotation_value
+                                )
                         if max_annotation:
-                            max_annotation = max(max_annotation.items(), key=lambda x: x[1])
+                            max_annotation = max(
+                                max_annotation.items(), key=lambda x: x[1]
+                            )
                             if max_annotation[1] >= 1:
                                 annotation = max_annotation[0]
 
@@ -86,31 +136,47 @@ class TAREAN_post_processing:
                         clusters.append(cluster)
 
         return clusters, metrics
-    #corrects number of analyzed reads by excluding contamination
+
+    # corrects number of analyzed reads by excluding contamination
     def correct_metrics(self, metrics, clusters):
         contamination = 0
         for cluster in clusters:
             if cluster["Annotation"] == "Contamination":
                 contamination += int(cluster["Number of reads"])
-        self.metrics["Number of analyzed reads"] = int(metrics["Number of analyzed reads"]) - contamination
+        self.metrics["Number of analyzed reads"] = (
+            int(metrics["Number of analyzed reads"]) - contamination
+        )
 
     def estimate_copy_number(self, clusters, metrics, g_size):
         for cluster in clusters:
-            cluster["Proportion"] = (float(cluster["Proportion"]) / self.metrics["Number of analyzed reads"]) * 100
+            cluster["Proportion"] = (
+                float(cluster["Number of reads"])
+                / self.metrics["Number of analyzed reads"]
+            ) * 100
             consensus_size = len(cluster["consensus"])
             if consensus_size > 0:
-                estimated_copy_number = ((g_size * float(cluster["Number of reads"])) / 100) / consensus_size
+                estimated_copy_number = (
+                    (g_size * float(cluster["Number of reads"])) / 100
+                ) / consensus_size
             else:
                 estimated_copy_number = 0
             cluster["Estimated copy number"] = estimated_copy_number
 
     def write_files(self, clusters, metrics):
-        with open(f"{self.output_base}.tsv", "w") as output_file, open(f"{self.output_base}.fasta", "w") as fasta_out:
-            output_file.write("Cluster\tProportion\tNumber of reads\tSAT prob\tconsensus\tC-index\tP-index\tAnnotation\tClassification\tEstimated copy number\n")
+        with open(f"{self.output_base}.tsv", "w") as output_file, open(
+            f"{self.output_base}.fasta", "w"
+        ) as fasta_out:
+            output_file.write(
+                "Cluster\tProportion\tNumber of reads\tSAT prob\tconsensus\tC-index\tP-index\tAnnotation\tClassification\tEstimated copy number\n"
+            )
             for cluster in clusters:
-                output_file.write(f"{cluster['Cluster']}\t{cluster['Proportion']}\t{cluster['Number of reads']}\t{cluster['SAT prob']}\t{cluster['consensus']}\t{cluster['C-index']}\t{cluster['P-index']}\t{cluster['Annotation']}\t{cluster['Classification']}\t{cluster['Estimated copy number']}\n")
+                output_file.write(
+                    f"{cluster['Cluster']}\t{cluster['Proportion']}\t{cluster['Number of reads']}\t{cluster['SAT prob']}\t{cluster['consensus']}\t{cluster['C-index']}\t{cluster['P-index']}\t{cluster['Annotation']}\t{cluster['Classification']}\t{cluster['Estimated copy number']}\n"
+                )
                 if cluster["consensus"] != "":
-                  fasta_out.write(f">{cluster['Cluster']}\t{cluster['Classification']}\n{cluster['consensus']}\n")
+                    fasta_out.write(
+                        f">{cluster['Cluster']}\t{cluster['Classification']}\n{cluster['consensus']}\n"
+                    )
 
     def overwrited_annotation(self, repeatM_file, clusters):
         with open(repeatM_file, "r") as infile:
@@ -139,11 +205,86 @@ class TAREAN_post_processing:
             cluster_seq = cluster["consensus"]
             for seq in fasta_seqs:
                 if cluster_seq == seq.seq:
-                    cluster["Annotation"] = f"{seq.id}"
-
+                    cluster["Annotation"] = "Satellite"
 
     def plot_clusters(self):
-        subprocess.run(["plot_clusters.R", f"{self.output_base}.tsv", f"plot{self.output_base}.png"])
+        subprocess.run(
+            [
+                "plot_clusters.R",
+                f"{self.output_base}.tsv",
+                f"repeat_stats_{self.output_base}.tsv",
+                f"cluster_plot_{self.output_base}.png",
+                f"stat_plot_{self.output_base}.png",
+            ]
+        )
+
+    def repeat_statistics(self, clusters):
+        stats = defaultdict(int)
+        with open(f"repeat_stats_{self.output_base}.tsv", "w") as out:
+            out.write("Type\tProportion\n")
+            # sum proportions of repetitive elements
+            for cluster in clusters:
+                anno = correct_names(cluster["Annotation"])
+                stats[anno] += cluster["Proportion"]
+
+            # now write them
+            for item in stats:
+                out.write(f"{item}\t{stats[item]}\n")
+            non_repetitive = 100 - sum(stats.values())
+            out.write(f"NA\t{non_repetitive}")
+
+    # correction for TE names just for plotting
+    def plot_correction(self, clusters):
+        with open("temp_correction.tsv", "w") as temp:
+            temp.write(
+                "Cluster\tProportion\tNumber of reads\tSAT prob\tconsensus\tC-index\tP-index\tAnnotation\tClassification\tEstimated copy number\n"
+            )
+            for cluster in clusters:
+                anno = correct_names(cluster["Annotation"])
+                temp.write(
+                    f"{cluster['Cluster']}\t{cluster['Proportion']}\t{cluster['Number of reads']}\t{cluster['SAT prob']}\t{cluster['consensus']}\t{cluster['C-index']}\t{cluster['P-index']}\t{anno}\t{cluster['Classification']}\t{cluster['Estimated copy number']}\n"
+                )
+            self.plot_clusters()
+            os.remove("temp_correction.tsv")
+
+
+# Hashing of TE names for annotation correction
+def correct_names(name):
+    hash = {
+        "LTR": "LTR",
+        "Helitron": "Helitron",
+        "LINE": "LINE",
+        "Organelle": "Organelle",
+        "organelle": "Organelle",
+        "rDNA": "rDNA",
+        "DTM": "Mutator",
+        "DTA": "hAT",
+        "DTT": "Tc-Mar",
+        "DTP": "P",
+        "DTB": "PiggyBac",
+        "DTH": "PIF-Harbinger",
+        "DTC": "CMC-Transib",
+        "TcMar-Tc1": "TcMar",
+        "SINE": "SINE",
+        "MITE": "MITE",
+        "CMC-Transib": "CMC",
+        "TcMar-Mariner": "TcMar",
+        "DT[0-9]+": "TIR",
+        "Bel-Pao": "LTR",
+        "Ty1_copia": "LTR",
+        "Ty3_gypsy": "LTR",
+        "TIR": "TIR",
+        "hAT": "hAT",
+        "PIF-Harbinger": "PIF-Harbinger",
+        "CMC": "CMC",
+        "Mutator": "Mutator",
+        "TcMar": "TcMar",
+    }
+    for key in hash.keys():
+        match = re.search(key, name)
+        if match:
+            return hash[key]
+    return name
 
 
 def main():
@@ -151,13 +292,12 @@ def main():
     html_file = args.html_file
     output_base = args.o
     g_size = args.g_size
-    #sanity checking custom annotation files
-
+    # sanity checking custom annotation files
 
     tarean = TAREAN_post_processing(html_file, output_base, g_size)
     tarean.correct_metrics(tarean.metrics, tarean.clusters)
     tarean.estimate_copy_number(tarean.clusters, tarean.metrics, g_size)
-    #if custom library is chosen and provided
+    # if custom library is chosen and provided
     if args.custom_anno and args.repeatM_file:
         repeatM_file = args.repeatM_file
         tarean.overwrited_annotation(repeatM_file, tarean.clusters)
@@ -169,10 +309,12 @@ def main():
         tarean.sat_annotation(tarean.clusters, fasta_sats)
     elif args.custom_sat and not args.fasta_sats:
         print("Provide a valid fasta file")
-
+    tarean.repeat_statistics(tarean.clusters)
     tarean.write_files(tarean.clusters, tarean.metrics)
 
     if args.plot:
-        tarean.plot_clusters()
+        tarean.plot_correction(tarean.clusters)
+
 
 main()
+
